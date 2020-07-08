537ce807c5d411cb4b65d052572ff5504174ef12
vanhelmont <54476474+vanhelmont@users.noreply.github.com>
2020-07-07 23:03:10 -0300

    Modify "Guess the number" game
    Change a "while" loop with a "for" loop, for more clarity.

--- guess_number/guess_number.py
+++ guess_number/guess_number.py
@@ -4,18 +4,17 @@ from random import randint
 def game(first_case, last_case, max_chances):
 	# Assigns a value to be guessed, taken into account limits imported in the function.
 	answer = randint(first_case, last_case + 1)
-	chance_left = max_chances
 
 	print('Number between {0} and {1} has been selected. \n'.format(str(first_case), str(last_case)))
 
-	while chance_left > 0:
+	for chance in range(max_chances):
+		chance_left = max_chances - chance
 		guess = input('You have {0} chances left. Make a guess: '.format(str(chance_left)))
 
 		# TO-DO: Verify that user input is valid
 
 		if int(guess) == answer:
-			guesses_taken = max_chances - chance_left + 1
-			print('You won! You achieved the result in {0} guesses. \n'.format(str(guesses_taken)))
+			print('You won! You achieved the result in {0} guesses. \n'.format(str(chance + 1)))
 			break
 		elif int(guess) > answer:
 			print('The answer is lower than {0}. \n'.format(str(guess)))
@@ -25,8 +24,6 @@ def game(first_case, last_case, max_chances):
 			print('The answer is higher than {0}. \n'.format(str(guess)))
 			if chance_left == 1:
 				defeat_msg(answer)
-
-		chance_left -= 1
 
 	print('Thanks for playing "Guess the Number"! \n')
 
